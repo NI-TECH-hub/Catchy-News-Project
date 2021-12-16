bfe34a904a7b0164e1e82ababf2fe80b460e794e
NI-TECH-hub <nikhilchinchore2002@gmail.com>
2021-12-16 23:10:43 +0530

    Initial Update

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import React, { useState } from 'react'
+import Navbar from './components/Navbar';
+import LoadingBar from 'react-top-loading-bar';
+import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+// class based component
+const App = () => {
+
+  const pageSize = 6;
+  const apiKey = process.env.REACT_APP_NEWS_API
+
+  const [progress, setProgress] = useState(0)
+
+  return (
+    <div>
+
+      <Router>
+        <Navbar />
+
+        {/* Top loader */}
+        <LoadingBar
+          height={3}
+          color='#f11946'
+          progress={progress}
+        />
+
+        {/* Categories */}
+        <Switch>
+          <Route exact path="/"><News setProgress={setProgress} apiKey={apiKey} key="general" category="general" country="in" pageSize={pageSize} /></Route>
+          <Route exact path="/business"><News setProgress={setProgress} apiKey={apiKey} key="business" category="business" country="in" pageSize={pageSize} /></Route>
+          <Route exact path="/entertainment"><News setProgress={setProgress} apiKey={apiKey} key="entertainment" category="entertainment" country="in" pageSize={pageSize} /></Route>
+          <Route exact path="/health"><News setProgress={setProgress} apiKey={apiKey} key="health" category="health" country="in" pageSize={pageSize} /></Route>
+          <Route exact path="/science"><News setProgress={setProgress} apiKey={apiKey} key="science" category="science" country="in" pageSize={pageSize} /></Route>
+          <Route exact path="/sports"><News setProgress={setProgress} apiKey={apiKey} key="sports" category="sports" country="in" pageSize={pageSize} /></Route>
+          <Route exact path="/technology"><News setProgress={setProgress} apiKey={apiKey} key="technology" category="technology" country="in" pageSize={pageSize} /></Route>
+        </Switch>
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Link, useLocation } from "react-router-dom";
+
+const Navbar = () => {
+
+    let location = useLocation();
+
+    return (
+        <div>
+            <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark ">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">Catchy News</Link>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
+                                <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} aria-current="page" to="/">Home</Link>
+                            </li>
+
+                            <li className="nav-item">
+                                {/* <Link className="nav-link" to="/business"> Business</Link> */}
+                                <Link className={`nav-link ${location.pathname === "/business" ? "active" : ""}`} aria-current="page" to="/business">Business</Link>
+                            </li>
+
+                            <li className="nav-item">
+                                {/* <Link className="nav-link" to="/entertainment"> Entertainment</Link> */}
+                                <Link className={`nav-link ${location.pathname === "/entertainment" ? "active" : ""}`} aria-current="page" to="/entertainment">Entertainment</Link>
+                            </li>
+
+                            <li className="nav-item">
+                                {/* <Link className="nav-link" to="/health"> Health</Link> */}
+                                <Link className={`nav-link ${location.pathname === "/health" ? "active" : ""}`} aria-current="page" to="/health">Health</Link>
+                            </li>
+
+                            <li className="nav-item">
+                                {/* <Link className="nav-link" to="/science"> Science</Link> */}
+                                <Link className={`nav-link ${location.pathname === "/science" ? "active" : ""}`} aria-current="page" to="/science">Science</Link>
+                            </li>
+
+                            <li className="nav-item">
+                                {/* <Link className="nav-link" to="/sports"> Sports</Link> */}
+                                <Link className={`nav-link ${location.pathname === "/sports" ? "active" : ""}`} aria-current="page" to="/sports">Sports</Link>
+                            </li>
+
+                            <li className="nav-item">
+                                {/* <Link className="nav-link" to="/technology"> Technology</Link> */}
+                                <Link className={`nav-link ${location.pathname === "/technology" ? "active" : ""}`} aria-current="page" to="/technology">Technology</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+        </div>
+    )
+}
+
+export default Navbar
